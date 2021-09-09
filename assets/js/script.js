0e2b1b7ab4fe5add7b2f7383c95e3de9c5effbe5
saleh88hbg <saleh.hbg@gmail.com>
2021-09-09 17:18:57 +0000

    Add script.js making a request

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const API_KEY = "DX7pl6cIUbj0dQ1XL3O7PtQtnCA";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
+}
